9ad6152fe65bc500a6f695676aa650da6d720ea4
François Févotte <ff@triscale-innov.com>
2019-06-16 00:43:48 +0200

    Initial version

---
+++ util.jl
@@ -0,0 +1,27 @@
+function read_data(fname)
+    open(fname) do f
+        n, cond, ref = split(readline(f), " ")
+        cond, ref = parse.(Float64, (cond, ref))
+        x = parse.(Float64, eachline(f))
+        cond, ref, x
+    end
+end
+
+const TESTDIR = "dataSum"
+
+function test(fun, typ)
+    cond = Float64[]
+    nsig = Float64[]
+    map(readdir(TESTDIR)) do fname
+        χ, ρ, x = read_data(joinpath(TESTDIR, fname))
+        σ = reliable_digits(ρ, x, fun, Val(typ))
+
+        push!(cond, χ)
+        push!(nsig, σ)
+    end
+
+    label = string(nameof(fun)) * ", " * string(typ)
+    scatter!(cond, nsig, label=label)
+end
+
+using Plots
