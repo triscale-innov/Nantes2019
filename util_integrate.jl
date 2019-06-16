2ced4c2336e90e7af255fcbaec3aa239fa41102c
François Févotte <ff@triscale-innov.com>
2019-06-16 13:18:15 +0200

    Comments

--- util_integrate.jl
+++ util_integrate.jl
@@ -1,4 +1,5 @@
 using Plots
+using BenchmarkTools
 
 function test(integrate_err, T)
     err = []
@@ -25,5 +26,85 @@ function test(integrate_err, T)
 
     p
 end
+
+
+
+
+
+
+
+"""
+Rational number, with numerator and denominator of type T
+"""
+struct MyRational{T<:Integer} <: Real
+    _num   :: T
+    _denom :: T
+
+    function MyRational{T}(n,d) where {T<:Integer}
+        d==0 && error("zero denominator")
+        new(n,d)
+    end
+end
+
+MyRational(num::T, denom::T) where {T} = MyRational{T}(num, denom)
+MyRational{T}(i::Int) where {T} = MyRational{T}(i, 1)
+
+
+num(a::MyRational)   = a._num
+denom(a::MyRational) = a._denom
+
+function my_gcd(a, b)
+    r = 1
+    while r != 0
+        r = mod(a, b)
+        (a, b) = (b, r)
+    end
+    a
+end
+
+function simplify_rational(num, denom)
+    if denom < 0
+        (num, denom) = (-num, -denom)
+    end
+    @assert denom > 0
+    d = my_gcd(num, denom)
+    MyRational(div(num, d), div(denom, d))
+end
+
+Base.promote_rule(::Type{MyRational{T}}, ::Type{Int}) where {T} = MyRational{T}
+
+import Base.*
+function *(a::MyRational, b::MyRational)
+    simplify_rational(num(a)*num(b), denom(a)*denom(b))
+end
+
+import Base./
+function /(a::MyRational, b::MyRational)
+    simplify_rational(num(a)*denom(b), denom(a)*num(b))
+end
+
+import Base.+
+function +(a::MyRational, b::MyRational)
+    simplify_rational(num(a)*denom(b)+num(b)*denom(a), denom(a)*denom(b))
+end
+
+Base.:-(a::MyRational) = MyRational(-num(a), denom(a))
+Base.:-(a::MyRational, b::MyRational) = a + (-b)
+
+import Base.<
+function <(a::MyRational, b::MyRational)
+    num(b) == 0 && return num(a) < 0
+    r = a / b
+    num(r) < denom(r)
+end
+
+import Base.==
+function ==(a::MyRational, b::MyRational)
+    num(a) == num(b) && denom(a) == denom(b)
+end
+
+Base.show(io::IO, a::MyRational{T}) where {T} = print(io, num(a), "/", denom(a))
+
+to_float(x::MyRational) = num(x)/denom(x)
 
 nothing
